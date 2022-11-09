1576dc845193b58daa67d1383fe48d247cafa26b
akbarimo <mo.akbari15@gmail.com>
2022-11-09 10:10:25 -0800

    Completed Recastly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,48 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+import Search from './Search.js';
+import searchYouTube from '../lib/searchYouTube.js';
+
+const App = () => {
+  const [videos, setVideos] = React.useState(exampleVideoData);
+
+  const [selectedVideo, updateVideo] = React.useState({
+    link: 'https://www.youtube.com/embed/dQw4w9WgXcQ',
+    title: 'Rick Astley - Never Gonna Give You Up (Official Music Video)',
+    description: 'The official video for “Never Gonna Give You Up” by Rick Astley'
+  });
+
+  let timeout = null;
+
+  const searchHandler = (e) => {
+    let query = e.target.value;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      searchYouTube(query, (videos => {
+        setVideos(videos);
+      }));
+    }, 500);
+  };
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <div><h5>Search<Search searchHandler={(e) => searchHandler(e)}/></h5></div>
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <div><h5>Video Player<VideoPlayer video={selectedVideo} videos={videos} /></h5></div>
+        </div>
+        <div className="col-md-5">
+          <div><h5>Video List<VideoList updateVideo={updateVideo} videos={videos} /></h5></div>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,19 +1,19 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+import VideoListEntry from './VideoListEntry.js';
 
-// PropTypes tell other developers what `props` a component expects
-// Warnings will be shown in the console when the defined rules are violated
+var VideoList = ({videos, updateVideo}) => {
+
+  return (
+    <div className="video-list">
+      {videos.map((video) => {
+        return <VideoListEntry updateVideo={updateVideo} video={video} key={video.id.videoId} />;
+      })}
+    </div>
+  );
+
+};
+
 VideoList.propTypes = {
   videos: PropTypes.array.isRequired,
 };
 
-// In the ES6 spec, files are "modules" and do not share a top-level scope.
-// `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
